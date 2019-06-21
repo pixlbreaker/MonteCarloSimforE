4363e94a52127b013b253a079f20fa9436e6a602
Mike Skotar <michaelskotar@gmail.com>
2019-06-21 00:05:45 -0400

    Added code to find the value of pi

---
+++ findingpi.py
@@ -0,0 +1,10 @@
+import random
+import math
+
+count_inside = 0
+for count in range(0, 10000):
+    d = math.hypot(random.random(), random.random())
+    print('d :' + str(d))
+    if d < 1: count_inside += 1
+count += 1
+print( 4.0 * count_inside / count)
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-t = np.arange(0.0, 2.0, 0.01)
-s = np.e ** t
+x = np.arange(0.0, 3.0, 0.001)
+y = x
 
 fig, ax = plt.subplots()
-ax.plot(t,s)
+ax.plot(x,y)
 ax.set(xlabel='time (s)', ylabel='voltage (mV)', title='About as simple as it gets, folks')
 ax.grid()
 
