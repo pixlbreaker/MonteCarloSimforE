f8573552bd56b92d11fbf4578fd35ba24fa0255d
Mike Skotar <michaelskotar@gmail.com>
2019-06-22 17:12:17 -0400

    Got the first verison of calculating E

---
+++ CalculateEuler.py
@@ -0,0 +1,23 @@
+import numpy as np
+import random
+
+def SimulationSum():
+    '''
+    we take a random number from 0 to 1, then we take another one and add it to the first one and so on, while our sum is less than 1. ξ is a quantity of numbers taken. The mean value of ξ is the Euler's number, which is approximately 2,7182818284590452353602874713527…
+    '''
+    n = 0
+    sum = 0
+    while (sum  < 1):
+        sum += random.uniform(0,1)
+        n += 1
+    sumMean = np.mean(n)
+    return sumMean 
+
+def CalculateEuler(n):
+    '''
+    Number of times to run the check
+    '''
+    sumlist =[]
+    for i in range(0, n):
+        sumlist.append(SimulationSum())
+    return np.mean(sumlist)
--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-x = np.arange(0.0, 3.0, 0.001)
-y = x
+import CalculateEuler
 
-fig, ax = plt.subplots()
-ax.plot(x,y)
-ax.set(xlabel='time (s)', ylabel='voltage (mV)', title='About as simple as it gets, folks')
-ax.grid()
+# x = np.arange(0.0, 3.0, 0.001)
+# y = x
 
-fig.savefig('test.png')
-plt.show()
+# fig, ax = plt.subplots()
+# ax.plot(x,y)
+# ax.set(xlabel='time (s)', ylabel='voltage (mV)', title='About as simple as it gets, folks')
+# ax.grid()
+
+# fig.savefig('test.png')
+# plt.show()
 
 
 print("Hello World")
+
+
+
+print(CalculateEuler.CalculateEuler(100))
