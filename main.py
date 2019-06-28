cbbe81fd6fc41cecebfcbbc46e105bd000bcfd71
Michael Skotar <michaelskotar@Michaels-MacBook-Air.local>
2019-06-27 23:11:58 -0400

    First Attempt to having the Euler Animation, still not working live

--- main.py
+++ main.py
@@ -14,27 +14,32 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
 def animate(i):
-    graph_data = open('Testing/sample.txt', 'r').read()
-    lines = graph_data.split('\n')
+    #graph_data = open('Testing/sample.txt', 'r').read()
+    #lines = graph_data.split('\n')
     xs =[]
     ys =[]
 
-    for line in lines:
-        if len(line) > 1:
-            x, y = line.split(',')
-            xs.append(x)
-            ys.append(y)
+    for i in range(0, 1000):
+        value = CalculateEuler.CalculateEuler(i)
+        xs.append(i+1)
+        ys.append(value)
+        print("On run {0} the value is: {1}".format(i, value))
+    # for line in lines:
+    #     if len(line) > 1:
+    #         x, y = line.split(',')
+    #         xs.append(x)
+    #         ys.append(y)
     ax1.clear()
     ax1.plot(xs, ys)
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
 
-list = []
-for i in range(0, 1000):
-    value = CalculateEuler.CalculateEuler(i)
-    list.append(value)
-    print("On run {0} the value is: {1}".format(i, value))
-plt.plot(list)
-plt.show()
+# list = []
+# for i in range(0, 1000):
+#     value = CalculateEuler.CalculateEuler(i)
+#     list.append(value)
+#     print("On run {0} the value is: {1}".format(i, value))
+# plt.plot(list)
+# plt.show()
 #CalculateEuler.CalculateETest()
